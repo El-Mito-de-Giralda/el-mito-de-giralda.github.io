e99d55205badf39612225e7ade3dc7b48768e6bd
Jorge Miranda de la Quintana <70879770+El-Mito-de-Giralda@users.noreply.github.com>
2024-10-10 19:10:16 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [checker({typescript: true}), react()],
-    base: '/https://github.com/El-Mito-de-Giralda/el-mito-de-giralda.github.io.git/',
+    base: 'https://github.com/El-Mito-de-Giralda/el-mito-de-giralda.github.io.git',
 });
