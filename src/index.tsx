73c8788d99480099e04d9d19eb0028b2718e0d9b
Jorge Miranda <mirandajorge97@gmail.com>
2024-10-21 10:56:37 +0200

    Husky prettier eslint

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./mystyle.scss";
+import { App } from "./app";
+import {createRoot} from "react-dom/client";
+
+
+const RootElement = document.getElementById("root");
+const root= createRoot(RootElement);
+
+root.render(
+    <App />
+);
+    
